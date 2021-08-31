4f1aa678fbcc920f43ed246db9316d57d0c14304
michael jones <michael.jowen.jones@gmail.com>
2021-08-31 16:24:23 +0100

    riht

--- src/4-promise-resolve-type.ts
+++ src/4-promise-resolve-type.ts
@@ -3,8 +3,8 @@ import wrapInPromise from "./helpers/wrapInPromise";
 function printWithDelay(ms: number) {
   console.log("START of function body");
 
-  const promise = wrapInPromise({ wait: ms, value: "hello world!" });
-  // const promise = wrapInPromise({ wait: ms, value: ["hello", "world!"] });
+  // const promise = wrapInPromise({ wait: ms, value: "hello world!" });
+  const promise = wrapInPromise({ wait: ms, value: ["hello", "world!"] });
   // const promise = wrapInPromise({ wait: ms, value: ["hello", "world"].length });
   promise.then((resolveValue) => console.log(resolveValue));
 
